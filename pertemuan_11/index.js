cbfc2506a73c7ead8ec3427185a738b03d8e3272
romilpolii <romilpolii25@gmail.com>
2020-09-24 13:54:16 +0800

    Arrow Function

---
+++ pertemuan_11/index.js
@@ -0,0 +1,35 @@
+        // Arrow Function
+
+function ucapkanSalam() {
+    return "Selamat Pagi";
+};
+console.log(ucapkanSalam())
+
+
+    //metode arrow function
+    //1. Fungsi dengan parameter
+const penjumlahan = (bilangan1, bilangan2) =>{
+    const hasil = bilangan1 + bilangan2;
+    return hasil;
+};
+console.log(penjumlahan(3,4));
+
+
+    //2. Fungsi tanpa parameter
+const jenisAnjing = () => {
+    const anjing = ["Pug", "Bulldog", "Poodle"];
+    return anjing[Math.floor(Math.random()*(anjing.length))]
+}
+console.log(jenisAnjing())
+
+
+    //Implicit Return Value
+//yang biasa
+function greeting(nama){
+    return `Hi ${nama}`;
+}
+console.log(greeting("Romil"));
+
+//use arrow function
+const inisial = (nama) => `Hi ${nama}`;
+console.log(inisial("Romiil"));
